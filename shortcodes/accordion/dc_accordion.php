926d1ebb55b3590a93d72cf095745cb51b7c5ca8
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-07-12 15:40:51 -0500

    feat(accordion): Agrega dinamismo al shortcode de accordion

--- shortcodes/accordion/dc_accordion.php
+++ shortcodes/accordion/dc_accordion.php
@@ -1,52 +1,54 @@
 <?php
 add_shortcode('dc_accordion', 'dc_accordion_function');
 
-function dc_accordion_function()
+function dc_accordion_function($atts)
 {
+  // Los atributos aceptados y sus valores predeterminados
+  $attributes = shortcode_atts(
+    array(
+      'step'  => '00',
+    ),
+    $atts
+  );
+
   wp_enqueue_style('dc-accordion-style', get_stylesheet_directory_uri() . '/shortcodes/accordion/dc_accordion.css', array(), '1.0');
   wp_enqueue_script('dc-accordion-script', get_stylesheet_directory_uri() . '/shortcodes/accordion/dc_accordion.js', array('jquery'), null, true);
-  $shortcode = do_shortcode('[INSERT_ELEMENTOR id="3292"]');
+  $allSteps = get_field('steps_snl');
+  $numberStep = intval($attributes['step']);
+  $protocolSteps = $allSteps[$numberStep]['protocol_steps'];
+
   ob_start();
-  $html = '';
-  $html .= "
+  $protocolStepsHTML = '';
+  $isActive = true;
+  foreach ($protocolSteps as $protocolStep) {
+    $protocolStepsHTML .= dc_get_protocol_step_html($protocolStep, $isActive);
+    $isActive = false;
+  }
+  $html = "
     <div class='trem-team'>
-      <div class='trem-team__item active'>
-        <div class='trem-team__item__fig'>
-          <div class='trem-team__item__fig__text'>
-            <div class='dc__accordeon_step-letter'>a</div>
-            <div class='dc__accordion_step-title'>Collaboration</div>
-          </div>
-          <div class='trem-team__item__fig__caption'>
-            $shortcode
-          </div>
+      $protocolStepsHTML
+    </div>
+  ";
+  $html .= ob_get_clean();
+  return $html;
+}
+
+function dc_get_protocol_step_html($protocolStep, $isActive)
+{
+  ob_start();
+  $classActive = $isActive ? 'active' : '';
+  $letterStep = $protocolStep['letter_step'];
+  $titleStep = $protocolStep['title_step'];
+  $templateStep = $protocolStep['template_step'];
+  $html = "
+    <div class='trem-team__item $classActive'>
+      <div class='trem-team__item__fig'>
+        <div class='trem-team__item__fig__text'>
+          <div class='dc__accordeon_step-letter'>$letterStep</div>
+          <div class='dc__accordion_step-title'>$titleStep</div>
         </div>
-      </div>
-      <div class='trem-team__item'>
-        <div class='trem-team__item__fig'>
-          <div class='trem-team__item__fig__text'>
-            <div class='dc__accordeon_step-letter'>a</div>
-            <div class='dc__accordion_step-title'>Collaboration</div>
-          </div>
-          <div class='trem-team__item__fig__caption'>
-            <h3 class='trem-team__item__fig__caption__name'>JHON PRIETO</h3>
-            <p class='trem-team__item__fig__caption__desc'>
-              CoFundador y Director Comercial de Esto Es Tremendo S.A.S. una agencia de publicidad independiente 100% colombiana, ejecutora de ideas innovadoras, dedicada a la creación, desarrollo, producción y montaje de eventos y convenciones, así como activaciones, promociones y experiencias para productos, marcas y organizaciones.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class='trem-team__item'>
-        <div class='trem-team__item__fig'>
-          <div class='trem-team__item__fig__text'>
-            <div class='dc__accordeon_step-letter'>a</div>
-            <div class='dc__accordion_step-title'>Collaboration</div>
-          </div>
-          <div class='trem-team__item__fig__caption'>
-            <h3 class='trem-team__item__fig__caption__name'>JHON PRIETO</h3>
-            <p class='trem-team__item__fig__caption__desc'>
-              CoFundador y Director Comercial de Esto Es Tremendo S.A.S. una agencia de publicidad independiente 100% colombiana, ejecutora de ideas innovadoras, dedicada a la creación, desarrollo, producción y montaje de eventos y convenciones, así como activaciones, promociones y experiencias para productos, marcas y organizaciones.
-            </p>
-          </div>
+        <div class='trem-team__item__fig__caption'>
+          $templateStep
         </div>
       </div>
     </div>
