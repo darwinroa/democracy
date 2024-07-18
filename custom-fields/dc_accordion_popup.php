92b72538552f163b9ac2400d3b0aa4825917fd0f
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-07-18 10:18:28 -0500

    feat(custom field): Actualiza ID de la página — South North Learning (SNL)

--- custom-fields/dc_accordion_popup.php
+++ custom-fields/dc_accordion_popup.php
@@ -247,7 +247,7 @@ if (function_exists('acf_add_local_field_group')) :
         array(
           'param' => 'page',
           'operator' => '==',
-          'value' => '2566',
+          'value' => '4719',
         ),
       ),
     ),
