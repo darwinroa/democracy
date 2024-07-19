2b8075c5610a96ac27e91acf0e8fdcf3432b04de
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-07-19 15:39:56 -0500

    docs(pkay button): Documenta el shortcode para el icono de play

--- shortcodes/play-video/dc_play_video.php
+++ shortcodes/play-video/dc_play_video.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+ * Imprime un botón con un ícono de play.
+ * Este shortcode se usa en el loop de projects para mostrar un botón de play
+ * a los proyectos que contienen un video.
+ * [button_play_video]
+ */
 add_shortcode('button_play_video', 'button_play_video_function');
 function button_play_video_function()
 {
@@ -12,8 +19,8 @@ function button_play_video_function()
   $isVideo = get_field('is_video');
   if ($isVideo) {
     $post_id = get_the_ID();
-    $svgFile = get_stylesheet_directory_uri() . '/inc/img/play-button.svg';
-    $iconPlay = file_get_contents($svgFile);
+    $svgFile = get_stylesheet_directory_uri() . '/inc/img/play-button.svg'; // Obtiene el svg del ícono de play
+    $iconPlay = file_get_contents($svgFile); // Convierte el svg en una imagen para mostrar en el html
     ob_start();
     $html = "<div data-id='$post_id' class='dc_play_button dc_video_pop_up'>$iconPlay</div>";
     $html .= ob_get_clean();
@@ -21,4 +28,8 @@ function button_play_video_function()
   }
 }
 
+/**
+ * Shortcode que imprime el html del pop up
+ * [html_popup]
+ */
 add_shortcode('html_popup', 'dc_html_popup');
