a29270414b8b5b900406f16a26154075f147664a
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-07-30 17:57:48 -0500

    feat: Agrega funcionalidad de mostrar y ocultar el contenido de las cards

--- inc/scripts/dc_scripts.js
+++ inc/scripts/dc_scripts.js
@@ -2,9 +2,35 @@ jQuery(document).ready(function($) {
   function viewMoreExpand() {
     // Usa la delegación de eventos para manejar el evento click en los elementos dinámicamente añadidos
     $(document).on('click', '.boton-expandir', function() {
-      // Encuentra el elemento hermano con la clase 'contenido-expandible' y toglea la clase 'mostrar'
-      $(this).next('.contenido-expandible').toggleClass('mostrar');
+      var $expandirButton = $(this);
+      var $contenidoExpandible = $expandirButton.next('.contenido-expandible');
+      
+      // Oculta el botón expandir
+      $expandirButton.hide();
+      
+      // Muestra el contenido expandible y añade la clase 'mostrar'
+      $contenidoExpandible.addClass('mostrar');
+      
+      // Asegúrate de mostrar el botón de ocultar
+      $contenidoExpandible.find('.boton-ocultar').show();
+    });
+
+    // Delegación de eventos para el botón de ocultar que es hijo del contenido-expandible
+    $(document).on('click', '.contenido-expandible .boton-ocultar', function() {
+      var $ocultarButton = $(this);
+      var $contenidoExpandible = $ocultarButton.closest('.contenido-expandible');
+      var $expandirButton = $contenidoExpandible.prev('.boton-expandir');
+
+      // Oculta el contenido expandible y quita la clase 'mostrar'
+      $contenidoExpandible.removeClass('mostrar');
+
+      // Muestra el botón expandir
+      $expandirButton.show();
+      
+      // Asegúrate de ocultar el botón de ocultar
+      $ocultarButton.hide();
     });
   }
+
   viewMoreExpand();
 });
