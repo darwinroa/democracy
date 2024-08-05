94d482fbae168a4248a62d13a8bb9f9c6c9f8dc9
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-08-05 10:07:27 -0500

    feat(accordeon): Agrega la clase active al tab seleccionado

--- shortcodes/accordion/dc_accordion.js
+++ shortcodes/accordion/dc_accordion.js
@@ -4,5 +4,19 @@ jQuery(document).ready(function($) {
       $('.trem-team__item').removeClass('active');
       $(this).addClass('active');
     });
+
+    // Delegación de eventos para elementos '.bdt-ep-fancy-tabs-item' dentro de los elementos '#step_02_a', '#step_02_b', '#step_02_c'
+    $('.dc__tabs_popup').on('click', '.bdt-ep-fancy-tabs .bdt-ep-fancy-tabs-item', function(event) {
+      // Obtener el elemento en el que se hizo clic
+      var clickedElement = $(event.delegateTarget);
+      
+      // Determinar cuál de los pasos se ha clicado
+      var stepID = clickedElement.attr('id');
+      
+      // Obtener el data-id del elemento .bdt-ep-fancy-tabs-item que fue clicado
+      var dataID = $(this).data('id');
+
+      $('#' + stepID + ' #' + dataID).addClass('active');
+    });
   });
 });
